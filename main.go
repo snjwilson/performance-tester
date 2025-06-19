3f059d932c8412711295a6f920120ef77eed9991
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2025-06-19 15:31:04 +0930

    Add load test script and integrate with main performance tester

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("Performance tester initializing...")
+	cmd := exec.Command("k6", "run", "load-test.ts")
+
+	// Capture the output of the command
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error while running k6 binary:", err)
+		return
+	}
+
+	// Print the output
+	fmt.Println("Command Output:")
+	fmt.Println(string(output))
+
+	fmt.Println("Performance Tester successfully completed!")
 }
